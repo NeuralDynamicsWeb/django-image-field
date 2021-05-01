997798eaf1838469ed7f7ceb8335c7b371d2aa2a
Nickolay Krykunov <nik.krikunov.99@gmail.com>
2021-05-01 22:19:06 +0300

    add base admin model with media

--- django_image_field/admin.py
+++ django_image_field/admin.py
@@ -1,14 +1,37 @@
 #region				-----External Imports-----
 from django.contrib.admin import (ModelAdmin, register)
+from django.forms import Media
 #endregion
 
 #region				-----Internal Imports-----
 from .models import (GeneralSetting, ImageSubSetting)
 #endregion
 
+
+_image_form_media = Media(
+    css={
+        'all': (
+            'django_image_field/admin/parler_admin.css',
+        )
+    },
+    js={
+        'all': (
+            'django_image_field/admin/dropzone.js',
+            'django_image_field/admin/index.js'
+        )
+    }
+)
+
+
 @register(ImageSubSetting)
 class ImageSubSettingAdmin(ModelAdmin):
-    list_filter=[""]
+    list_filter=[]
 
 @register(GeneralSetting)
 class GeneralSettingAdmin(ModelAdmin): pass
+
+
+class BaseImageFieldAdmin(ModelAdmin):
+    @property
+    def media(self):
+        return base_media + _image_form_media
